b1275f03f76289f1f5063ae18e644c86cdd2a772
Olaf <olafnieliwodzki@wp.pl>
2023-12-21 01:49:31 +0100

    Fix max-widths and initial responsive mobile about section

--- src/components/organisms/AboutSection/index.tsx
+++ src/components/organisms/AboutSection/index.tsx
@@ -1,20 +1,21 @@
 import MaxWidthWrapper from '~components/atoms/MaxWidthWrapper';
 import styles from './styles.module.scss';
 import TextReveal from '~components/atoms/TextReveal';
-
-const aboutPhrases = [
-    `Hi im Olaf. Front-end and animation enthusiast`,
-    `and full-stack programmer. Creates dynamic,`,
-    `engaging websites from concept to completion.`,
-    `Let's bring your ideas to life!`,
-];
+import Image from 'next/image';
 
 const AboutSection = () => {
     return (
         <section className={styles.aboutSectionContainer}>
             <MaxWidthWrapper>
                 <div className={styles.aboutContentContainer}>
-                    <div className={styles.aboutFirstImage}></div>
+                    <div className={styles.aboutFirstImage}>
+                        <Image
+                            src={'/olaf-nieliwodzki-1.jpg'}
+                            alt={'Olaf Nieliwodzki Freelance Developer'}
+                            width={237}
+                            height={300}
+                        />
+                    </div>
                     <div className={styles.aboutTextContainer}>
                         <TextReveal
                             className={styles.aboutText}
@@ -22,7 +23,14 @@ const AboutSection = () => {
                             options={{once: true}}
                         />
                     </div>
-                    <div className={styles.aboutSecondImage}></div>
+                    <div className={styles.aboutSecondImage}>
+                        <Image
+                            src={'/olaf-nieliwodzki-2.jpg'}
+                            alt={'Olaf Nieliwodzki Freelance Developer'}
+                            width={509}
+                            height={719}
+                        />
+                    </div>
                 </div>
             </MaxWidthWrapper>
         </section>
